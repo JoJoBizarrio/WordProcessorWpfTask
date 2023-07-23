0619883b538a32094578cddfbff07714e6de2065
Константин <kon.ivanyuk@gmail.com>
2023-07-23 19:08:02 +0700

    + saveFileDalog need replace dialog from ViewModel

--- WordProcessingWpfTask/ViewModel/MainWindowViewModel.cs
+++ WordProcessingWpfTask/ViewModel/MainWindowViewModel.cs
@@ -6,6 +6,7 @@ using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
@@ -22,10 +23,12 @@ namespace WordProcessingWpfTask.ViewModel
 			OpenAsync = new AsyncCommand(OnOpenExecutedAsync);
 			RemoveWordsAsync = new AsyncCommand(OnRemoveWordsExecutedAsync);
 			RemoveSeparatorsAsync = new AsyncCommand(OnRemoveSeparatorExecutedAsync);
+			SaveAsync = new AsyncCommand(OnSaveExecutedAsync);
 		}
 
 		private readonly IRedactor _redactor;
 
+		private string _filePath { get; set; }
 		private string _currentText { get; set; }
 		public string CurrentText
 		{
@@ -45,6 +48,8 @@ namespace WordProcessingWpfTask.ViewModel
 
 		public IAsyncCommand RemoveSeparatorsAsync { get; }
 
+		public IAsyncCommand SaveAsync { get; }
+
 		public ICommand Quit { get; } = new RelayCommand(p => Application.Current.Shutdown());
 
 		async public Task OnOpenExecutedAsync()
@@ -52,7 +57,7 @@ namespace WordProcessingWpfTask.ViewModel
 			OpenFileDialog openFileDialog = new OpenFileDialog();
 			openFileDialog.Title = "Choose file";
 			openFileDialog.InitialDirectory = Environment.CurrentDirectory;
-			openFileDialog.Filter = "Text (*.txt)|*.txt|WinWord (*.docx)|*.docx|All files (*.*)|*.*";
+			openFileDialog.Filter = "Text (*.txt)|*.txt|Word (*.docx)|*.docx|All files (*.*)|*.*";
 
 			if (openFileDialog.ShowDialog() == false)
 			{
@@ -61,6 +66,7 @@ namespace WordProcessingWpfTask.ViewModel
 
 			using (StreamReader streamReader = new StreamReader(openFileDialog.FileName))
 			{
+				_filePath = openFileDialog.FileName;
 				CurrentText = null;
 				CurrentText = await streamReader.ReadToEndAsync();
 			}
@@ -70,13 +76,19 @@ namespace WordProcessingWpfTask.ViewModel
 		{
 			if (string.IsNullOrEmpty(LettersCount))
 			{
-				MessageBox.Show("Please, write number", "Value is empty");
+				MessageBox.Show("Please, enter count of letters", "Value of letter's count is wrong");
 				return;
 			}
 
 			if (!int.TryParse(LettersCount, out int lettersCount))
 			{
-				MessageBox.Show("It isnt number", "Value is wrong");
+				MessageBox.Show("Value of letter's count isnt number", "Value of letter's count is wrong");
+				return;
+			}
+
+			if (lettersCount == 0)
+			{
+				MessageBox.Show("Letter's count is zero", "Value of letter's count is wrong");
 				return;
 			}
 
@@ -88,18 +100,12 @@ namespace WordProcessingWpfTask.ViewModel
 			}
 
 			var temp = CurrentText;
+			temp = await _redactor.RemoveWordsParallelAsync(temp, lettersCount);
 
-			if (lettersCount != 0)
-			{
-				temp = await _redactor.RemoveWordsParallelAsync(temp, lettersCount);
-			}
-
-			LettersCount = null;
-			LettersCount = "Count of letter";
 			CurrentText = null;
 			CurrentText = temp;
 
-			MessageBox.Show($"Worlds with count of letter = {lettersCount} removed", "Result", MessageBoxButton.OK, MessageBoxImage.Information, MessageBoxResult.OK);
+			MessageBox.Show($"Worlds with count of letter = {lettersCount} removed", "Done", MessageBoxButton.OK, MessageBoxImage.Information, MessageBoxResult.OK);
 		}
 
 		async public Task OnRemoveSeparatorExecutedAsync()
@@ -115,17 +121,40 @@ namespace WordProcessingWpfTask.ViewModel
 			CurrentText = null;
 			CurrentText = await _redactor.RemoveAllSeparatorsParallelAsync(temp);
 
-			MessageBox.Show("All separators removed", "Result", MessageBoxButton.OK, MessageBoxImage.Information, MessageBoxResult.OK);
+			MessageBox.Show("All separators removed", "Done", MessageBoxButton.OK, MessageBoxImage.Information, MessageBoxResult.OK);
+		}
+
+		async public Task OnSaveExecutedAsync()
+		{
+			SaveFileDialog saveFileDialog = new SaveFileDialog()
+			{
+				DefaultExt = ".txt",
+				AddExtension = true,
+				Title = "Save file",
+				CreatePrompt = true,
+				OverwritePrompt = true,
+				Filter = "Text (*.txt)|*.txt|Word (*.docx)|*.docx|All files (*.*)|*.*",
+				InitialDirectory = _filePath
+			};
+
+			if (saveFileDialog.ShowDialog() == false)
+			{
+				return;
+			}
+
+			using (var writer = new StreamWriter(new FileStream(saveFileDialog.FileName, FileMode.Create, FileAccess.Write)))
+			{
+				await writer.WriteAsync(CurrentText);
+			}
+
+			_filePath = saveFileDialog.FileName;
 		}
 
 		public event PropertyChangedEventHandler PropertyChanged;
 
 		public void OnPropertyChanged([CallerMemberName] string propertyName = "")
 		{
-			if (PropertyChanged != null)
-			{
-				PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-			}
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 		}
 	}
 }
