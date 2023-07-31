9e54b6de897df3d8b5403710af6fb344ac1f3355
Константин <kon.ivanyuk@gmail.com>
2023-08-01 02:00:29 +0700

    + mock dosnt work. why?

--- WordProcessorWpfTask.Tests/ViewModelTests.cs
+++ WordProcessorWpfTask.Tests/ViewModelTests.cs
@@ -1,21 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Moq;
+using WordProcessingWpfTask.Abstract;
+using WordProcessingWpfTask.Model;
 using WordProcessingWpfTask.ViewModel;
 
 namespace WordProcessorWpfTask.Tests
 {
 	[TestFixture]
+	[TestOf(typeof(MainWindowViewModel))]
 	public class ViewModelTests
 	{
 		private MainWindowViewModel _viewModel;
-
+		private Mock<IRedactor> _redactorMock;
 		[SetUp]
 		public void Setup()
 		{
-			_viewModel = new MainWindowViewModel(null);
+			
+			
+		}
+
+		[Test]
+		async public Task OpenAsync_ExpectedNewTextFileInCollection()
+		{
+			_redactorMock = new Mock<IRedactor>();
+			var result = new Task<TextFile>(() => { return new TextFile(); });
+			_redactorMock.Setup(redactor => redactor.OpenFileAsync(null)).Returns(result).Verifiable();
+			_viewModel = new MainWindowViewModel(_redactorMock.Object);
+			await _viewModel.OpenAsync.ExecuteAsync(string.Empty);
+
+			//_redactorMock.Verify();
+			Assert.IsTrue(_viewModel.TextFilesCollection.Count == 1);
+		}
+
+		[Test]
+		async public Task RemoveMarks_Execute()
+		{
+			var textFiles = new TextFile[4] { new TextFile() { IsEditable = true }, new TextFile(), new TextFile(), new TextFile() };
+			var expected = textFiles[0];
+			var result = new Task<IEnumerable<TextFile>>(() => textFiles.Where(item => item.IsEditable == true));
+			foreach (TextFile textFile in textFiles)
+			{
+				_viewModel.TextFilesCollection.Add(textFile);
+			}
+
+			_redactorMock.Setup(redactor => redactor.RemoveAllMarksInSeveralTextFilesParallelAsync(null))
+						 .Returns(result)
+						 .Verifiable();
+
+			//act
+			await _viewModel.RemoveMarksAsync.ExecuteAsync();
+
+			_redactorMock.Verify(x => x.RemoveAllMarksInSeveralTextFilesParallelAsync(null));
 		}
 	}
 }
