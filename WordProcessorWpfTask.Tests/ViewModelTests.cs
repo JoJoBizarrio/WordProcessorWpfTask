759fdf429befbe4c98292342eb530d98e4ec0927
Константин <kon.ivanyuk@gmail.com>
2023-08-01 02:11:39 +0700

    + moq dosnt work debug: pass througth moq-object but dont verify

--- WordProcessorWpfTask.Tests/ViewModelTests.cs
+++ WordProcessorWpfTask.Tests/ViewModelTests.cs
@@ -22,12 +22,12 @@ namespace WordProcessorWpfTask.Tests
 		async public Task OpenAsync_ExpectedNewTextFileInCollection()
 		{
 			_redactorMock = new Mock<IRedactor>();
-			var result = new Task<TextFile>(() => { return new TextFile(); });
+			var result = new Task<TextFile>(() => { return new TextFile() {Text="mytext" }; });
 			_redactorMock.Setup(redactor => redactor.OpenFileAsync(null)).Returns(result).Verifiable();
 			_viewModel = new MainWindowViewModel(_redactorMock.Object);
 			await _viewModel.OpenAsync.ExecuteAsync(string.Empty);
 
-			//_redactorMock.Verify();
+			_redactorMock.Verify();
 			Assert.IsTrue(_viewModel.TextFilesCollection.Count == 1);
 		}
 
