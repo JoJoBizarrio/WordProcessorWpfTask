31ec46c921ce5421e13498d7cfa89a2d51c24c91
Константин <kon.ivanyuk@gmail.com>
2023-08-03 20:50:52 +0700

    + injected ikriv solution for lack vizualizator of  tabControl

--- WordProcessingWpfTask/View/MainWindow.xaml.cs
+++ WordProcessingWpfTask/View/MainWindow.xaml.cs
@@ -7,84 +7,67 @@ using System.Windows.Controls;
 
 namespace WordProcessingWpfTask.View
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        private List<object> _storage = new List<object>();
+	/// <summary>
+	/// Логика взаимодействия для MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
 
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+		public string FilePath
+		{
+			get => (string)GetValue(FilePathProperty);
+			set => SetValue(FilePathProperty, value);
+		}
 
-        public string FilePath
-        {
-            get => (string)GetValue(FilePathProperty);
-            set => SetValue(FilePathProperty, value);
-        }
+		public static readonly DependencyProperty FilePathProperty = DependencyProperty.Register(
+			nameof(FilePath),
+			typeof(string),
+			typeof(MainWindow),
+			new PropertyMetadata(default(string)));
 
-        public static readonly DependencyProperty FilePathProperty = DependencyProperty.Register(
-            nameof(FilePath),
-            typeof(string),
-            typeof(MainWindow),
-            new PropertyMetadata(default(string)));
+		private void OpenMenuItem_Click(object sender, RoutedEventArgs e)
+		{
+			var openFileDialog = new OpenFileDialog()
+			{
+				Title = "Choose file",
+				DefaultExt = ".txt",
+				Filter = "Text (*.txt)|*.txt|All files (*.*)|*.*"
+			};
 
-        private void OpenMenuItem_Click(object sender, RoutedEventArgs e)
-        {
-            var openFileDialog = new OpenFileDialog()
-            {
-                Title = "Choose file",
-                DefaultExt = ".txt",
-                Filter = "Text (*.txt)|*.txt|All files (*.*)|*.*"
-            };
+			if (openFileDialog.ShowDialog() == true)
+			{
+				FilePath = openFileDialog.FileName;
+			}
+			else
+			{
+				FilePath = null;
+			}
+		}
 
-            if (openFileDialog.ShowDialog() == true)
-            {
-                FilePath = openFileDialog.FileName;
-            }
-            else
-            {
-                FilePath = null;
-            }
-        }
+		private void SaveMenuItem_Click(object sender, RoutedEventArgs e)
+		{
+			var saveFileDialog = new SaveFileDialog()
+			{
+				Title = "Save file",
+				DefaultExt = ".txt",
+				AddExtension = true,
+				CreatePrompt = false,
+				OverwritePrompt = false,
+				Filter = "Text (*.txt)|*.txt|All files (*.*)|*.*"
+			};
 
-        private void SaveMenuItem_Click(object sender, RoutedEventArgs e)
-        {
-            var saveFileDialog = new SaveFileDialog()
-            {
-                Title = "Save file",
-                DefaultExt = ".txt",
-                AddExtension = true,
-                CreatePrompt = false,
-                OverwritePrompt = false,
-                Filter = "Text (*.txt)|*.txt|All files (*.*)|*.*"
-            };
-
-            if (saveFileDialog.ShowDialog() == true)
-            {
-                FilePath = saveFileDialog.FileName;
-            }
-            else
-            {
-                FilePath = null;
-            }
-        }
-
-        private void RedactorTabControl_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
-        {
-            var send = sender as TabControl;
-            send.SelectedItem;
-            if (!_storage.Contains(e.AddedItems))
-            {
-                _storage.Add(e.Source);
-            }
-            else
-            {
-
-                send.SelectedItem = _storage.Find(item => item.Equals(e.Source));
-            }
-        }
-    }
+			if (saveFileDialog.ShowDialog() == true)
+			{
+				FilePath = saveFileDialog.FileName;
+			}
+			else
+			{
+				FilePath = null;
+			}
+		}
+	}
 }
