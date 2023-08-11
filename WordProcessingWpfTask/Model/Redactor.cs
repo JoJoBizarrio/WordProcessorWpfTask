dcd9558aebf789e625f6457d29cf308338a25336
Константин <kon.ivanyuk@gmail.com>
2023-08-11 20:03:52 +0700

    +

--- WordProcessingWpfTask/Model/Redactor.cs
+++ WordProcessingWpfTask/Model/Redactor.cs
@@ -6,7 +6,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using WordProcessingWpfTask.Abstract;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace WordProcessingWpfTask.Model
 {
